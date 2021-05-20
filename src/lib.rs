eefc538896078ee57588f113f1815e01edfab6d2
Oleksandr Kravchenko <zartarn15@gmail.com>
2021-05-20 10:51:04 +0300

    Rename enc.rs -> x509.rs
    Change-Id: I5a5fc8417281ff19e2d0b2569431628722abfda2

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 //! This is a simple library for creating X509 certificates.
 //! The library is based on simple_asn1: https://crates.io/crates/simple_asn1
 
-pub mod enc;
+pub mod x509;
 pub mod ext;
 
-pub use enc::*;
+pub use x509::*;
 pub use ext::*;
